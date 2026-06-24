b26fd1f89c0e7361ab60296715ad0262fd8dd1fa
ABHISEK DASH <im.abhisekdash.9753@gmail.com>
2026-06-24 19:53:11 +0530

    chore(deps): install zustand for client state management

--- src/proxy.js
+++ src/proxy.js
@@ -4,20 +4,22 @@ import { jwtVerify } from 'jose';
 export async function proxy(request) {
     const url = request.nextUrl.pathname;
 
+    if (url == '/admin/login' || url == '/api/admin/login') return NextResponse.next();
+
     if (url.startsWith('/admin') || url.startsWith('/api/admin')) {
 
         const token = request.cookies.get('admin_token')?.value;
-        if (!token) return NextResponse.redirect(new URL('/login', request.url));
+        if (!token) return NextResponse.redirect(new URL('/admin/login', request.url));
 
         try {
             const secret = new TextEncoder().encode(process.env.JWT_SECRET);
             const { payload } = await jwtVerify(token, secret);
 
             if (payload.role !== 'admin') {
-                return NextResponse.redirect(new URL('/login', request.url));
+                return NextResponse.redirect(new URL('/admin/login', request.url));
             }
         } catch (error) {
-            return NextResponse.redirect(new URL('/login', request.url));
+            return NextResponse.redirect(new URL('/admin/login', request.url));
         }
     }
 
